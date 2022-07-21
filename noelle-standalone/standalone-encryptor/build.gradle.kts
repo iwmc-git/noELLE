85ca10e0717d773f572aac94094a45ac2ed6d228
nightlyDuck <adambushev@gmail.com>
2022-07-21 05:06:35 -0800

    Bump bouncy castle provider to 1.71 (bcprov-jdk18on)

--- noelle-standalone/standalone-encryptor/build.gradle.kts
+++ noelle-standalone/standalone-encryptor/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     compileOnlyApi("at.favre.lib:bcrypt:0.9.0")
-    compileOnlyApi("org.bouncycastle:bcprov-jdk15on:148")
+    compileOnlyApi("org.bouncycastle:bcprov-jdk18on:1.71")
     compileOnlyApi("com.google.guava:guava:31.1-jre")
 
     compileOnly("org.jetbrains:annotations:23.0.0")
